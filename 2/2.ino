81fffc2a0865fc557e77bb451d282bee8c2e1cd0
timetraveller05 <anubhavmishra.chimpu@gmail.com>
2019-07-22 18:03:36 +0530

    updated a few codes

--- 2/2.ino
+++ 2/2.ino
@@ -1,22 +1,13 @@
-int LED1 =13;
-int LED2 =12;
-int LED3 =11;
-int LED4 =10;
-int LED5 =9;
-int LED6 =8;
-//int LED7 = 8;
-//int LED8 = 8;
+int led[8] = {13, 12, 11, 10, 9, 8, 7, 6};
 
 
 void setup() 
 {
-        // put your setup code here, to run once:
-    pinMode(LED1, OUTPUT);
-    pinMode(LED2, OUTPUT);
-    pinMode(LED3, OUTPUT);
-    pinMode(LED4, OUTPUT);
-    pinMode(LED5, OUTPUT);
-    pinMode(LED6, OUTPUT);
+    // put your setup code here, to run once:
+    for(int i = 0; i < 8; i++)
+    {
+      pinMode(led[i], OUTPUT);
+    }
 
 
 
@@ -25,37 +16,12 @@ void setup()
 void loop() 
 {
        
-       // put your main code here, to run repeatedly:
-    digitalWrite(LED1, HIGH);
-     delay(175);
-    digitalWrite(LED1,LOW); 
-    
-    digitalWrite(LED2, HIGH);
-     delay(175);
-    digitalWrite(LED2,LOW); 
-    
-    digitalWrite(LED3, HIGH);
-     delay(175);
-    digitalWrite(LED3,LOW); 
-    
-    digitalWrite(LED4, HIGH);
-     delay(175);
-    digitalWrite(LED4,LOW); 
-    
-    digitalWrite(LED5, HIGH);
-     delay(175);
-    digitalWrite(LED5,LOW); 
-    
-    digitalWrite(LED6, HIGH);
-     delay(175);
-    digitalWrite(LED6,LOW); 
-    
-    /*digitalWrite(LED7, HIGH);
-     delay(1000);
-    digitalWrite(LED2.LOW); 
-    
-    digitalWrite(LED1, HIGH);
-     delay(1000);
-    digitalWrite(LED2.LOW);*/ 
+    // put your main code here, to run repeatedly:
+    for(int i = 0; i < 8; i++)
+    {
+      digitalWrite(led[i], HIGH);
+      delay(250);
+      digitalWrite(led[i], LOW);
+      delay(250);
+    }
 }
-
--- README.md
+++ README.md
@@ -1,22 +1,14 @@
 # Robotics
 This folder contains some codes written for arduino uno  projects(small).
 
-#_1
-----------------------------------------------------
-Code for alternate blinking of 10 LEDs connected to the arduino.
-
-
-#2
+#1
 ---------------------------------------------------- 
 Same code as above but with different delay and 8 LEDs.
 
-#3
+#2
 ----------------------------------------------------
 Code for LEDs blinking twice .
 
-#4
+#3
 ----------------------------------------------------
 Code for increasing and decreasing brightness of an  LED using analog output in Arduino.
-
-#6
-----------------------------------------------------
